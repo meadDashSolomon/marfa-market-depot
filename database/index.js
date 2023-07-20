cb7105fcc1501403640fca2f973bc5354181c339
Mead Solomon <mead93@gmail.com>
2023-07-20 18:56:37 -0500

    clean up file structure

--- database/index.js
+++
@@ -1,8 +0,0 @@
-// const mongoose = require('mongoose');
-
-// // connect to db
-// mongoose.connect('mongodb://127.0.0.1:27017/SDC', {
-//   useNewUrlParser: true, useUnifiedTopology: true
-// })
-//   .then(() => console.log('MongoDB connected...'))
-//   .catch(err => console.log('Could not connect to MongoDB...', err));
